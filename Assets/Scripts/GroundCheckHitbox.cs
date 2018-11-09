3458b05d0c081a04841c1fa490926f91367be8dd
Hampus <hampus.andersson@elev.ga.lbs.se>
2018-11-09 16:01:53 +0100

    Massa saker

---
+++ Assets/Scripts/GroundCheckHitbox.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheckHitbox : MonoBehaviour
+{
+    public bool isGrounded;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isGrounded = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isGrounded = false;
+    }
+
+
+}
