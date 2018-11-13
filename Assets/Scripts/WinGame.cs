e4ae87e09e2e29cab4394617a24163a3941b4bf1
Hampus <hampus.andersson@elev.ga.lbs.se>
2018-11-13 09:18:38 +0100

    spara
    sparar

---
+++ Assets/Scripts/WinGame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinGame : MonoBehaviour
+{
+    //public string sceneToLoad = "Level1";
+
+    private void OnTriggerEnter2D(Collider2D Collision)
+    {
+        if (Collision.tag == "Player")
+        {
+            SceneManager.LoadScene("Level1");
+        }
+    }
+}
